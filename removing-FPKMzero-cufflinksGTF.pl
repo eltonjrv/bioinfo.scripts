c34dbc5aa6d6c9609a36f8731033bc7c27da4f89
Elton Vasconcelos <eltonjrv@gmail.com>
2018-05-16 16:33:32 -0700

    Update removing-FPKMzero-cufflinksGTF.pl

--- removing-FPKMzero-cufflinksGTF.pl
+++ removing-FPKMzero-cufflinksGTF.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl 
 # Programmer: Elton Vasconcelos (28.Dec.2015)
 # Script that does not print features with FPKM = 0.0000000000 ib the transcripts.gtf cufflinks output file
-# Usage: perl removing-FPKMzero-cufflinksGTF.pl transcripts.gff 1>outfile.gtf 2>FPKMzero.gtf
+# Usage: perl removing-FPKMzero-cufflinksGTF.pl transcripts.gtf 1>outfile.gtf 2>FPKMzero.gtf
 #################################################################################################
 
 open (INFILE, "$ARGV[0]") or die ("Can't open file $ARGV[0]!\n");
