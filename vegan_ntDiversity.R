7419737181af837428cce58e4845ee060cd048a6
Elton Vasconcelos <eltonjrv@gmail.com>
2018-05-16 17:21:47 -0700

    Update vegan_ntDiversity.R

--- vegan_ntDiversity.R
+++ vegan_ntDiversity.R
@@ -1,7 +1,11 @@
+# R script that calculates shannon entropy values for each column of a MSA
+# Programmer: Elton Vasconcelos (20/Apr/2017)
+# Usage: Rscript vegan_ntDiversity.R [output from baseCount_perMSAcolumn.pl script]
+#############################################################################
 library(vegan)
 args <- commandArgs(TRUE)
 x = read.delim(args[1], row.names=1)
 x[is.na(x)] = 0
 xdiv = diversity(x)
-out = gsub("-bc.tab", "-div.tab", args[1])
+out = gsub(".tab", "-div.tab", args[1])     # Edit here according to your input file extension (if it is not .tab, replace by what it is)
 write.table(xdiv, file = out, sep = "\t", quote = FALSE, col.names = FALSE)
