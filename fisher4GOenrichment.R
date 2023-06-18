54b56b0d22bd48700ae5736b9b3118ac686473da
Elton Vasconcelos <eltonjrv@gmail.com>
2023-06-18 21:27:36 +0100

    Update fisher4GOenrichment.R

--- fisher4GOenrichment.R
+++ fisher4GOenrichment.R
@@ -14,4 +14,11 @@ i = 1
 n = dim(x)
 glist = as.numeric(c(args[2]))
 total_genes_wGO = as.numeric(c(args[3]))
-for (i in c(i:n)) {onList_onGO = x[i,2]; onList_notGO = glist - x[i,2]; onGO_notList = x[i,3] - x[i,2]; notList_notGO = total_genes_wGO - onList_onGO - onList_notGO - onGO_notList; y <- matrix(c(onList_onGO, onList_notGO, onGO_notList, notList_notGO), 2, 2, byrow=T); print(fisher.test(y)$p.value);}
+for (i in c(i:n)) {
+  onList_onGO = x[i,2];
+  onList_notGO = glist - x[i,2];
+  onGO_notList = x[i,3] - x[i,2];
+  notList_notGO = total_genes_wGO - onList_onGO - onList_notGO - onGO_notList;
+  y <- matrix(c(onList_onGO, onList_notGO, onGO_notList, notList_notGO), 2, 2, byrow=T);
+  print(fisher.test(y)$p.value);
+}
